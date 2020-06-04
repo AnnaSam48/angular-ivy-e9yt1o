8f05e66210aa8a4487c8b19436a806340cb76282
AnnaSam48 <littlesamurai4@yahoo.co.uk>
2020-06-04 14:25:26 +0300

    Observable added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
-import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
-import { EmployeeService } from '../employee.service';
-import { HttpClientModule} from '@angular/common/http';
+import { EmployeeService } from './employee.service';
+
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, EmployeeListComponent, EmployeeDetailComponent ],
+  imports:      [ BrowserModule, HttpClientModule, FormsModule ],
+  declarations: [ AppComponent, EmployeeListComponent,EmployeeDetailComponent ],
   providers:    [EmployeeService], 
   /*register employee service to app module so all components
   that need it can access it*/
